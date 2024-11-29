26ab43bb82573965203feb591354cb30b659ea3b
Sigmaraven <masterofthecheese@gmail.com>
2024-11-29 09:56:57 -0500

    Combat art framework

--- The Unity War.ltproj/resources/custom_components/custom_skill_components.py
+++ The Unity War.ltproj/resources/custom_components/custom_skill_components.py
@@ -22,3 +22,25 @@ class PersonalSkill(SkillComponent):
     nid = 'personal_skill'
     desc = "A filler for future organizational purposes."
     tag = SkillTags.ATTRIBUTE
+    
+class CostUses(SkillComponent):
+    nid = 'cost_uses'
+    desc = "Skill costs alternate number of weapon uses. Unit must have at least that many uses."
+    tag = SkillTags.CHARGE
+
+    expose = ComponentType.Int
+    value = 2
+
+    ignore_conditional = True
+
+    def condition(self, unit, item):
+        return item.data.get('uses', 999) > self.value
+            
+    def on_hit(self, actions, playback, unit, item, target, item2, target_pos, mode, attack_info):
+        actions.append(action.SetObjData(item, 'uses', item.data['uses'] - self.value + 1))
+
+    def on_miss(self, actions, playback, unit, item, target, item2, target_pos, mode, attack_info):
+        if item.uses_options.lose_uses_on_miss():
+            actions.append(action.SetObjData(item, 'uses', item.data['uses'] - self.value + 1))
+        else:
+            actions.append(action.SetObjData(item, 'uses', item.data['uses'] - self.value))
