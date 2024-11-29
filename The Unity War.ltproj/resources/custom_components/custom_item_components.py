88a1522a703fdb7a96597996353f4a0f42825183
Sigmaraven <masterofthecheese@gmail.com>
2024-11-28 21:48:44 -0500

    Possibly done with intro cutscene?

--- The Unity War.ltproj/resources/custom_components/custom_item_components.py
+++ The Unity War.ltproj/resources/custom_components/custom_item_components.py
@@ -17,3 +17,18 @@ class DoNothing(ItemComponent):
 
     expose = ComponentType.Int
     value = 1
+
+class StatusAfterCombat(ItemComponent):
+    nid = 'status_after_combat'
+    desc = "Target will gain the specified status at the end of combat. Prevents changes being applied mid-combat."
+    tag = ItemTags.SPECIAL
+
+    expose = ComponentType.Skill  # Nid
+
+    def end_combat(self, playback, unit, item, target, item2, mode):
+        act = action.AddSkill(target, self.value, unit)
+        action.do(act)
+
+    def ai_priority(self, unit, item, target, move):
+        # Do I add a new status to the target
+        return ai_status_priority(unit, target, item, move, self.value)
