38d3486499ea0e2a232256b7469b84b3cd2c7a20
Sigmaraven <masterofthecheese@gmail.com>
2025-01-04 16:34:51 -0500

    Chapter 4 intro stuff

--- The Unity War.ltproj/resources/custom_components/custom_skill_components.py
+++ The Unity War.ltproj/resources/custom_components/custom_skill_components.py
@@ -9,6 +9,7 @@ from app.engine.game_state import game
 from app.engine.objects.unit import UnitObject
 from app.utilities import utils, static_random
 from app.utilities.enums import Strike
+import app.engine.combat.playback as pb
 
 class PersonalSkill(SkillComponent):
     nid = 'personal_skill'
@@ -99,3 +100,65 @@ class EndstepEvent(SkillComponent):
 
     def on_endstep(self, actions, playback, unit):
         game.events.trigger_specific_event(self.value, unit, None, unit.position, local_args={})
+
+class DynamicResistMultiplier(SkillComponent):
+    nid = 'dynamic_resist_multiplier'
+    desc = "Multiplies damage taken by a fraction"
+    tag = SkillTags.COMBAT
+
+    expose = ComponentType.String
+
+    def resist_multiplier(self, unit, item, target, item2, mode, attack_info, base_value):
+        from app.engine import evaluate
+        try:
+            local_args = {'item': item, 'item2': item2, 'mode': mode, 'skill': self.skill, 'attack_info': attack_info, 'base_value': base_value}
+            return float(evaluate.evaluate(self.value, unit, target, unit.position, local_args))
+        except Exception:
+            print("Couldn't evaluate %s conditional" % self.value)
+            return 1
+            
+class DefenseProcWhenHit(SkillComponent):
+    nid = 'defense_proc_when_hit'
+    desc = "Allows skill to proc when defending a single strike, only consumes charges when hit"
+    tag = SkillTags.ADVANCED
+
+    expose = ComponentType.Skill
+    _did_action = False
+    _got_hit = False
+
+    def start_sub_combat(self, actions, playback, unit, item, target, item2, mode, attack_info):
+        if mode == 'defense' and target and skill_system.check_enemy(unit, target):
+            if not get_weapon_filter(self.skill, unit, item):
+                return
+            proc_rate = get_proc_rate(unit, self.skill)
+            if static_random.get_combat() < proc_rate:
+                act = action.AddSkill(unit, self.value)
+                action.do(act)
+                if act.skill_obj:
+                    playback.append(pb.DefenseProc(unit, act.skill_obj))
+                self._did_action = True
+                
+    def after_take_strike(self, actions, playback, unit, item, target, item2, mode, attack_info, strike):
+        if strike != Strike.MISS:
+            self._got_hit = True
+
+    def end_sub_combat(self, actions, playback, unit, item, target, item2, mode, attack_info):
+        if self._did_action:
+            if self._got_hit:
+                action.do(action.TriggerCharge(unit, self.skill))
+            action.do(action.RemoveSkill(unit, self.value))
+        self._got_hit = False
+        self._did_action = False
+        
+def get_proc_rate(unit, skill) -> int:
+    for component in skill.components:
+        if component.defines('proc_rate'):
+            return component.proc_rate(unit)
+    return 100  # 100 is default
+
+
+def get_weapon_filter(skill, unit, item) -> bool:
+    for component in skill.components:
+        if component.defines('weapon_filter'):
+            return component.weapon_filter(unit, item)
+    return True
